af8ab7510c8f77f09670d47aae5cdb1ce4d5c2fe
Jarod <N00385903@upn.pe>
2025-06-15 22:37:49 -0500

    Clase Visitante con validación de DNI

---
+++ SistemaRegistroMuseos/src/sistemaregistromuseos/Visitante.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package sistemaregistromuseos;
+
+/**
+ *
+ * @author a-ntt
+ */
+public class Visitante {
+    private String dni;
+    private String nombres;
+    private String procedencia;
+}
